9c9b81cfa596088226e265fea53d79f983db1ccc
ana.camacho <129320167+anacamacho-cloud@users.noreply.github.com>
2024-06-25 12:10:51 -0300

    Banco de dados
    Coloquei na pasta do 'C:' e coloquei os códigos php: conector, cadastrar e registrar.

---
+++ registrar_movimento.php
@@ -0,0 +1,9 @@
+<?php
+//Vamos programar aqui
+require_once('./conectorMySQL.php');
+
+$nome_peca = $_GET['nome_peca'] ;
+$cor_peca = $_GET['cor_peca'] ;
+$coluna_peca = $_GET['coluna_peca'] ;
+$linha_peca = $_GET['linhad_peca'] ;
+?>
